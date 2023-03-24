9235f0ecbb5136df93aca2bb095844c7f4c1362a
bakkavamshi0171 <118384205+bakkavamshi0171@users.noreply.github.com>
2023-03-25 00:25:35 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,17 @@
+package edubridge;
+import java.util.Scanner;
+
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int N=sc.nextInt();
+		System.out.println(N);
+		System.out.println("Helloworld");
+		System.out.println("Welcome to Java Program");
+		
+	}
+
+}
